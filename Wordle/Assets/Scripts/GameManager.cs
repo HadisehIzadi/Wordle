a412d8ab225b74fe7ed994540dee495d821b2cdd
HadisehIzadi <110168673+HadisehIzadi@users.noreply.github.com>
2023-07-24 21:22:11 +0430

    user interface part1

---
+++ Wordle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public enum GameState { Menu, Game, LevelComplete, Gameover, Idle }
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+
+    [Header(" Settings ")]
+    private GameState gameState;
+
+
+    [Header(" Events ")]
+    public static Action<GameState> onGameStateChanged;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetGameState(GameState gameState)
+    {
+        this.gameState = gameState;
+        onGameStateChanged?.Invoke(gameState);
+    }
+}
